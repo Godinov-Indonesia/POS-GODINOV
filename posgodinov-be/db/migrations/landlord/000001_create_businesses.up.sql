7a38dd1aad70b466718ed9394ea2cb05b2b33271
RyyzDev <fachriakbarkutubi@gmail.com>
2026-09-04 23:03:31 +0700

    fix: add is_deleted to businesses migration

--- posgodinov-be/db/migrations/landlord/000001_create_businesses.up.sql
+++ posgodinov-be/db/migrations/landlord/000001_create_businesses.up.sql
@@ -5,5 +5,6 @@ CREATE TABLE businesses (
     password        VARCHAR(255) NOT NULL,
     name            VARCHAR(150) NOT NULL,
     owner_name      VARCHAR(100),
+    is_deleted      BOOLEAN DEFAULT FALSE,
     created_at      TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
